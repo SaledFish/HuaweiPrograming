341df7e46bb18b17f22978a404f6ffee20c84ef0
SaledFish <2289807686@qq.com>
2018-08-09 17:10:08 +0800

    8/9

---
+++ 2_CountChar.cpp
@@ -0,0 +1,23 @@
+/*
+写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数，不区分大小写
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	char cha;
+	while (cin >> str >> cha)
+	{
+		transform(str.begin(), str.end(), str.begin(), ::tolower);
+		tolower(cha);
+		int Count = count(str.begin(), str.end(), cha);
+		printf("%d", Count);
+	}
+	return 0;
+}
